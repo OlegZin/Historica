8d401186af9eb74d89fc5c2ea23d11386cc409a1
OlegZin <needleemon@yandex.ru>
2022-01-09 20:23:53 +0500

    1

---
+++ src/components/GameScreen/ObjectPanel.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-card class="pa-3">
+        <v-row>
+            <v-col>
+                <v-img
+                    v-if="currentCell && currentCell.object"
+                    :src="icons[typeModel]"
+                />
+                <v-img
+                    v-else
+                    :src="icons[typeModel]"
+                />
+            </v-col>
+            <v-col>
+                sgrddgh
+            </v-col>
+        </v-row>
+    </v-card>
+</template>
+
+<script>
+    import { mapState } from 'vuex'
+
+    export default {
+        name: 'GameField',
+
+        computed: {
+            ...mapState({
+                icons: (state) => state.icons,
+                currentCell: (state) => state.currentCell,
+            }),
+
+            typeModel() {
+                return this.currentCell?.object?.type || this.currentCell?.type
+            },
+        },
+    }
+</script>
---
+++ src/store/state/Objects.js
@@ -0,0 +1,47 @@
+export default ({
+    appletree: {
+        name: 'appletree',
+        icon: require('@/assets/objects/appletree.gif'),
+        count: 100,
+    },
+    bear: {
+        name: 'bear',
+        icon: require('@/assets/objects/bear.gif'),
+    },
+    bigtree: {
+        name: 'bigtree',
+        icon: require('@/assets/objects/bigtree.gif'),
+    },
+    bizon: {
+        name: 'bizon',
+        icon: require('@/assets/objects/bizon.gif'),
+    },
+    blackWolf: {
+        name: 'blackWolf',
+        icon: require('@/assets/objects/black_wolf.gif'),
+    },
+    grass: {
+        name: 'grass',
+        icon: require('@/assets/objects/grass.gif'),
+    },
+    mushrooms: {
+        name: 'mushrooms',
+        icon: require('@/assets/objects/mushrooms.gif'),
+    },
+    stoneBrovn: {
+        name: 'stoneBrovn',
+        icon: require('@/assets/objects/stone_brovn.gif'),
+    },
+    stoneGray: {
+        name: 'stoneGray',
+        icon: require('@/assets/objects/stone_gray.gif'),
+    },
+    tree: {
+        name: 'tree',
+        icon: require('@/assets/objects/tree.gif'),
+    },
+    wolf: {
+        name: 'wolf',
+        icon: require('@/assets/objects/wolf.gif'),
+    },
+})
